20bff1e39595cf0b7e60fd864ee8ddcae39df30e
Antony Potter <tone@potteringabout.net>
2024-07-13 12:18:17 +0000

    Some reformatting

--- provider.tf
+++ provider.tf
@@ -3,9 +3,9 @@ terraform {
   required_version = "~> 1.5"
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = "~> 5.0"
-      configuration_aliases = [aws.deployment]
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+      //configuration_aliases = [aws.deployment]
     }
   }
 }
