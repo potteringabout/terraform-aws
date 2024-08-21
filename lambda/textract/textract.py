c58f28d5a455480fb066527ed2dc149d55fa209f
Antony Potter <tone@potteringabout.net>
2024-08-21 22:23:26 +0000

    Added Textract function

--- lambda/textract/textract.py
+++ lambda/textract/textract.py
@@ -13,3 +13,24 @@ def lambda_handler(event, context):
     bucket = record['s3']['bucket']['name']
     key = unquote_plus(record['s3']['object']['key'])
     print(f"{bucket}/{key}")
+
+    textract = boto3.client('textract')
+
+    try:
+      response = textract.analyze_document(
+        Document={
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': key
+              }
+          },
+          FeatureTypes=[ 'TABLES','FORMS','LAYOUT']
+      )
+
+      for b in response["Blocks"]:
+        print(f"BlockType {b["BlockType"]} Confidence {b["Confidence"]} TextType {b["TextType"]}  Text {b["Text"]}")
+
+      return_result = {"Status": "Success"}
+      return return_result
+    except Exception as error:
+      return {"Status": "Failed", "Reason": json.dumps(error, default=str)}
