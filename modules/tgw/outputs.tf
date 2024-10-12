c3db77f1408a4a9afae62afd0ac426cb00ee053e
Antony Potter <tone@potteringabout.net>
2024-10-12 15:35:49 +0000

    Added TGW

--- modules/tgw/outputs.tf
+++ modules/tgw/outputs.tf
@@ -7,3 +7,13 @@ output "transit_gateway_arn" {
   description = "The ARN of the created Transit Gateway"
   value       = aws_ec2_transit_gateway.this.arn
 }
+
+output "inbound_route_table_id" {
+  description = "The ID of the inbound route table"
+  value       = aws_ec2_transit_gateway_route_table.inbound.id
+}
+
+output "outbound_route_table_id" {
+  description = "The ID of the outbound route table"
+  value       = aws_ec2_transit_gateway_route_table.outbound.id
+}
